304dc36d690d1fb035896ce9d521e99100f3625b
Kevin Dev <kevin.developer71@gmail.com>
2023-03-23 00:08:09 -0600

    basic UI structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 }
