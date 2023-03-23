f23b01017ed73a124222eef49de4d76c9b17a6ed
Kevin Dev <kevin.developer71@gmail.com>
2023-03-23 00:50:49 -0600

    get random pokemon id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
+import { getOptionsForVote } from '@/services/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const { data, isLoading } = trpc.hello.useQuery({ text: 'Kevin' });
+  const [[first, second], setIds] = useState<number[]>([0, 0]);
 
-  if (isLoading) return <div>Loading....</div>;
-
-  if (data) return <div>Hi!, {data.greeting}</div>;
+  useEffect(() => {
+    setIds(getOptionsForVote());
+  }, []);
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-200">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-200">{second}</div>
       </div>
     </div>
   );
