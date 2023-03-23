708ff4f997dc928d222651f50c022ead36990bf0
Kevin Dev <kevin.developer71@gmail.com>
2023-03-23 00:30:09 -0600

    add tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import type { AppProps } from "next/app"
+import type { AppProps } from 'next/app';
+import { trpc } from '@/utils/trpc';
 
-import "@/styles/globals.css"
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default trpc.withTRPC(App);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
+import { trpc } from '@/utils/trpc';
+
 export default function Home() {
+  const { data, isLoading } = trpc.hello.useQuery({ text: 'Kevin' });
+
+  if (isLoading) return <div>Loading....</div>;
+
+  if (data) return <div>Hi!, {data.greeting}</div>;
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
@@ -9,5 +17,5 @@ export default function Home() {
         <div className="w-16 h-16 bg-red-200" />
       </div>
     </div>
-  )
+  );
 }
